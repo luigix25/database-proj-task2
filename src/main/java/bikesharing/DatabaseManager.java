3ac81c801f0b3816cb29e81e17e673b825b34c96
giomba <giomba@glgprograms.it>
2020-02-01 15:18:38 +0100

    fixed remaining TODO; final code cleanup

--- src/main/java/bikesharing/DatabaseManager.java
+++ src/main/java/bikesharing/DatabaseManager.java
@@ -44,20 +44,20 @@ import com.mongodb.client.result.DeleteResult;
 public class DatabaseManager {
 
 	private static DatabaseManager instance = null;
-		
+
 	private final ServerAddress[] hosts = {
 		new ServerAddress("172.16.0.58",27017),
 		new ServerAddress("172.16.0.59",27017),
 		new ServerAddress("172.16.0.61",27017)
 	};
-	
+
 	private String mongoURL;
-		
+
 	private final String databaseName = "ducange";
-	
+
 	private MongoClient mongoClient;
 	private MongoDatabase database;
-	
+
 	private DatabaseManager() {
 		/* prepare URL for connection */
 		mongoURL = "mongodb://";
@@ -67,33 +67,33 @@ public class DatabaseManager {
 		}
 		mongoURL += "/?replicaSet=BSSReplica&readPreference=secondary";
 		System.err.println("[D] connecting to URL: " + mongoURL);
-		
+
 		/* actually connect */
 		mongoClient = MongoClients.create(mongoURL);
 		database = mongoClient.getDatabase(databaseName);
 	}
-	
+
 	public static DatabaseManager getInstance() {
 		if(instance == null) {
 			instance = new DatabaseManager();
 		}
-		
+
 		return instance;
 	}
-	
+
 	public static void close() {
 		if(instance == null)
 			return;
 		instance.mongoClient.close();
 	}
-	
+
 	public final ServerAddress[] getHosts() {
 		return this.hosts;
 	}
 
 	public User login(String username, String password) {
 		//Can't use secondary, because i need consistency
-		
+
 		Document d = database.getCollection("user").find(and(eq("username", username), eq("password", password))).first();
 		if (d != null) {
 			User user = new User();
@@ -105,7 +105,7 @@ public class DatabaseManager {
 		}
 		return null;
 	}
-	
+
 	public boolean insertDocument(String data, String collectionName) {
 		try {
 			Document document = Document.parse(data);
@@ -116,76 +116,76 @@ public class DatabaseManager {
 		}
 		return true;
 	}
-	
+
 	public boolean insertBatch(List<String> data) {
 		if (data == null) {
 			System.err.println("No Data");
 			return false;
 		}
-			
+
 		List<Document> documents = new ArrayList<Document>();
 
 		for(String json : data) {
 			Document doc;
 			try {
 				doc = Document.parse(json);
-				
+
 				Document time = (Document)doc.get("time");
-				
+
 				String ts_start = time.getString("timestamp_start");
 				String ts_end = time.getString("timestamp_end");
-				
+
 		        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-		        
+
 		        LocalDateTime localDate_start = LocalDateTime.parse(ts_start,formatter);
-		        LocalDateTime localDate_end = LocalDateTime.parse(ts_end,formatter);       
-		        
+		        LocalDateTime localDate_end = LocalDateTime.parse(ts_end,formatter);
+
 		        time.put("timestamp_start", localDate_start);
 		        time.put("timestamp_end", localDate_end);
 
 		        doc.put("time", time);
-		      		        
+
 			} catch(Exception e) {
 				e.printStackTrace();
 				return false;
 			}
-			documents.add(doc);			
+			documents.add(doc);
 		}
-		
+
 		final ClientSession clientSession = mongoClient.startSession();
-				
+
 		ReplaceOptions options = new ReplaceOptions();
 		options.upsert(true);
-		
+
 		for(Document doc : documents) {
-		
+
 			TransactionBody<Void> txnBody = new TransactionBody<Void>() {
 			    public Void execute() {
 			        MongoCollection<Document> trip = database.getCollection("trip");
 			        MongoCollection<Document> station = database.getCollection("station");
-			        
+
 			        Document space = (Document)doc.get("space");
-	
+
 			        Document station_start = null;
 			        Document station_end = null;
-			        
+
 			        if(space.containsKey("station_start")) {
 			        	station_start = new Document().append("city", doc.getString("city")).append("name", space.getString("station_start"));
 			        }
-			        
+
 			        if(space.containsKey("station_end")) {
 			        	station_end = new Document().append("city", doc.getString("city")).append("name", space.getString("station_end"));
 			        }
-			        			        
+
 			        //This one will be catched by the caller
 			        trip.insertOne(clientSession,doc);
-			        
+
 			        if(station_start != null)
 			        	station.replaceOne(station_start, station_start, options);
-			    
+
 			        if(station_end != null)
 			        	station.replaceOne(station_end, station_end, options);
-			        
+
 			        return null;
 			    }
 			};
@@ -194,16 +194,16 @@ public class DatabaseManager {
 			       Step 4: Use .withTransaction() to start a transaction,
 			       execute the callback, and commit (or abort on error).
 			    */
-	
+
 			    clientSession.withTransaction(txnBody);
 			} catch (RuntimeException e) {
 				e.printStackTrace();
 				return false;
-			} 
+			}
 		}
-		
+
 		clientSession.close();
-				
+
 		return true;
 	}
 
@@ -219,7 +219,6 @@ public class DatabaseManager {
 					new Document("$match", new Document("city", city)),
 					new Document("$set", new Document("stations", new BsonArray(Arrays.asList(new BsonString("$space.station_start"), new BsonString("$space.station_end"))))),
 					new Document("$project", new Document("stations", 1)),
-			        // new Document("$limit", 100), // TODO
 			        new Document("$group", new Document("_id", null).append("s", new Document("$addToSet", new Document("$arrayElemAt", new BsonArray(Arrays.asList(new BsonString("$stations"), new BsonInt32(0))))))));
 
 			AggregateIterable<Document> output = database.getCollection("trip").aggregate(pipeline);
@@ -252,16 +251,16 @@ public class DatabaseManager {
 
 		return true;
 	}
-	
+
 	public List<String> getStationsForCity(String city) {
 		List<Bson> pipeline = Arrays.asList(
 				new Document("$match", new Document("city", city))
 				);
 		AggregateIterable<Document> output = database.getCollection("station").aggregate(pipeline);
-		
+
 		if (output == null)
 			return null;
-		
+
 		MongoCursor<Document> cursor = output.cursor();
 		List<String> stations = new ArrayList<String>();
 		while (cursor.hasNext()) {
@@ -273,14 +272,14 @@ public class DatabaseManager {
 
 		if (stations.isEmpty())
 			return null;
-		
+
 		Collections.sort(stations, (d1, d2) -> {
 			return Integer.parseInt(d1.substring(4)) - Integer.parseInt(d2.substring(4));
 		});
-				
+
 		return stations;
 	}
-	
+
 
 	//AnyTime
 	public List<Document> tripsForEachCity(String collectionName) {
@@ -288,28 +287,28 @@ public class DatabaseManager {
 		project.add(Projections.excludeId());
 		project.add(Projections.include("trips"));
 		project.add(new Document("city","$_id"));
-		
+
 		List<Bson> pipeline = Arrays.asList(
 				Aggregates.group("$city", Accumulators.sum("trips", 1)),
-				Aggregates.project(Projections.fields(project))		
+				Aggregates.project(Projections.fields(project))
 		);
-		
+
 		MongoCollection<Document> collection = database.getCollection(collectionName).withReadPreference(ReadPreference.secondaryPreferred());
-		
+
 		MongoCursor<Document> cursor = collection.aggregate(pipeline).iterator();
-		
+
 		ArrayList<Document> result = new ArrayList<Document>();
-		
+
 		while(cursor.hasNext()) {
 			result.add(cursor.next());
 		}
-		
+
 		cursor.close();
-		
+
 		return result;
-		
+
 	}
-	
+
 	// trips for each city for various years
 	public List<Document> tripsForCityDuringYears(String city) {
 		ArrayList<Field<?>> addYear = new ArrayList<Field<?>>();
@@ -336,36 +335,34 @@ public class DatabaseManager {
 		project.add(Projections.excludeId());
 		project.add(Projections.include("trips"));
 		project.add(new Document("city","$_id"));
-		
+
 		ArrayList<Field<?>> addYear = new ArrayList<Field<?>>();
 		addYear.add(new Field<Document>("year",new Document("$year","$time.timestamp_start")));
-		
+
 		List<Bson> pipeline = Arrays.asList(
 				Aggregates.addFields(addYear),
 				Aggregates.match(new Document("year",year)),
 				Aggregates.group("$city", Accumulators.sum("trips", 1)),
-				Aggregates.project(Projections.fields(project))		
+				Aggregates.project(Projections.fields(project))
 		);
-		
+
 		MongoCollection<Document> collection = database.getCollection(collectionName).withReadPreference(ReadPreference.secondaryPreferred());
-		
+
 		MongoCursor<Document> cursor = collection.aggregate(pipeline).iterator();
-		
+
 		ArrayList<Document> result = new ArrayList<Document>();
-		
+
 		while(cursor.hasNext()) {
 			result.add(cursor.next());
 		}
-		
+
 		cursor.close();
-		
+
 		return result;
-		
+
 	}
-	
+
 	//Trips for a given city
-	// TODO: can be optimized!!!
-	
 	public List<Document> tripsForACity(String city, String collectionName){
 		List<Bson> project = new ArrayList<Bson>();
 		project.add(Projections.excludeId());
@@ -377,7 +374,7 @@ public class DatabaseManager {
 		ArrayList<Field<?>> addDates = new ArrayList<Field<?>>();
 		addDates.add(new Field<Document>("year",new Document("$year","$time.timestamp_start")));
 		addDates.add(new Field<Document>("month",new Document("$month","$time.timestamp_start")));
-		
+
 		List<Bson> pipeline = Arrays.asList(
 				//Filter by City
 				Aggregates.match(new Document("city",city)),
@@ -394,21 +391,21 @@ public class DatabaseManager {
 		);
 
 		MongoCollection<Document> collection = database.getCollection(collectionName).withReadPreference(ReadPreference.secondaryPreferred());
-		
+
 		MongoCursor<Document> cursor = collection.aggregate(pipeline).iterator();
-		
+
 		ArrayList<Document> result = new ArrayList<Document>();
-		
+
 		while(cursor.hasNext()) {
 			result.add(cursor.next());
 		}
-		
+
 		cursor.close();
-		
+
 		return result;
-		
+
 	}
-	
+
 	public List<Document> tripsPerCityYear(String city,int year,String collectionName){		//on a monthly basis
 		List<Bson> project = new ArrayList<Bson>();
 		project.add(Projections.excludeId());
@@ -420,7 +417,7 @@ public class DatabaseManager {
 		ArrayList<Field<?>> addDates = new ArrayList<Field<?>>();
 		addDates.add(new Field<Document>("year",new Document("$year","$time.timestamp_start")));
 		addDates.add(new Field<Document>("month",new Document("$month","$time.timestamp_start")));
-		
+
 		List<Bson> pipeline = Arrays.asList(
 				//Filter by City
 				Aggregates.match(new Document("city",city)),
@@ -439,27 +436,27 @@ public class DatabaseManager {
 		);
 
 		MongoCollection<Document> collection = database.getCollection(collectionName).withReadPreference(ReadPreference.secondaryPreferred());
-		
+
 		MongoCursor<Document> cursor = collection.aggregate(pipeline).iterator();
-		
+
 		ArrayList<Document> result = new ArrayList<Document>();
-		
+
 		while(cursor.hasNext()) {
 			result.add(cursor.next());
 		}
-		
+
 		cursor.close();
-		
+
 		return result;
-		
+
 	}
-	
+
 	public List<Document> tripsPerGender(String city, int year, String collectionName){
 		List<Bson> projections = new ArrayList<Bson>();
 		projections.add(Projections.excludeId());
 		projections.add(Projections.include("count"));
 		projections.add(new Document("gender","$_id"));
-		
+
 		List<Bson> pipeline = Arrays.asList(
 				Aggregates.match(Filters.eq("city", city)),
 				Aggregates.addFields(new Field<Document>("year",new Document("$year","$time.timestamp_start"))),
@@ -467,97 +464,97 @@ public class DatabaseManager {
 				Aggregates.group("$rider.gender", Accumulators.sum("count", 1)),
 				Aggregates.project(Projections.fields(projections))
 		);
-		
+
 		MongoCollection<Document> collection = database.getCollection(collectionName).withReadPreference(ReadPreference.secondaryPreferred());
 		MongoCursor<Document> cursor = collection.aggregate(pipeline).iterator();
 		ArrayList<Document> result = new ArrayList<Document>();
-		
+
 		while(cursor.hasNext()) {
 			result.add(cursor.next());
 		}
-		
+
 		cursor.close();
-		
+
 		return result;
 	}
-	
+
 	public List<Document> tripsPerGender(String city, String collectionName){
 		List<Bson> projections = new ArrayList<Bson>();
 		projections.add(Projections.excludeId());
 		projections.add(Projections.include("count"));
 		projections.add(new Document("gender","$_id"));
-		
+
 		List<Bson> pipeline = Arrays.asList(
 				Aggregates.match(Filters.eq("city", city)),
 				Aggregates.group("$rider.gender", Accumulators.sum("count", 1)),
 				Aggregates.project(Projections.fields(projections))
 		);
-		
+
 		MongoCollection<Document> collection = database.getCollection(collectionName).withReadPreference(ReadPreference.secondaryPreferred());
 		MongoCursor<Document> cursor = collection.aggregate(pipeline).iterator();
 		ArrayList<Document> result = new ArrayList<Document>();
-		
+
 		while(cursor.hasNext()) {
 			result.add(cursor.next());
 		}
-		
+
 		cursor.close();
-		
+
 		return result;
 	}
-	
+
 	public List<Document> tripsPerGender(int year, String collectionName){
 		List<Bson> projections = new ArrayList<Bson>();
 		projections.add(Projections.excludeId());
 		projections.add(Projections.include("count"));
 		projections.add(new Document("gender","$_id"));
-		
+
 		List<Bson> pipeline = Arrays.asList(
 				Aggregates.addFields(new Field<Document>("year",new Document("$year","$time.timestamp_start"))),
 				Aggregates.match(Filters.eq("year", year)),
 				Aggregates.group("$rider.gender", Accumulators.sum("count", 1)),
 				Aggregates.project(Projections.fields(projections))
 		);
-		
+
 		MongoCollection<Document> collection = database.getCollection(collectionName).withReadPreference(ReadPreference.secondaryPreferred());
 		MongoCursor<Document> cursor = collection.aggregate(pipeline).iterator();
 		ArrayList<Document> result = new ArrayList<Document>();
-		
+
 		while(cursor.hasNext()) {
 			result.add(cursor.next());
 		}
-		
+
 		cursor.close();
-		
+
 		return result;
 	}
-	
+
 	public List<Document> tripsPerGender(String collectionName){
 		List<Bson> projections = new ArrayList<Bson>();
 		projections.add(Projections.excludeId());
 		projections.add(Projections.include("count"));
 		projections.add(new Document("gender","$_id"));
-		
+
 		List<Bson> pipeline = Arrays.asList(
 				Aggregates.group("$rider.gender", Accumulators.sum("count", 1)),
 				Aggregates.project(Projections.fields(projections))
 		);
-		
+
 		MongoCollection<Document> collection = database.getCollection(collectionName).withReadPreference(ReadPreference.secondaryPreferred());
 		MongoCursor<Document> cursor = collection.aggregate(pipeline).iterator();
 		ArrayList<Document> result = new ArrayList<Document>();
-		
+
 		while(cursor.hasNext()) {
 			result.add(cursor.next());
 		}
-		
+
 		cursor.close();
-		
+
 		return result;
 	}
-	
+
 	public List<User> getAllUsers(){
-		
+
 		List<User> users = new ArrayList<User>();
 		MongoCursor<Document> cursor = database.getCollection("user").find().iterator();
 		while(cursor.hasNext()) {
@@ -568,37 +565,37 @@ public class DatabaseManager {
 				user.setStatus((String)document.get("status"));
 				user.setUsername((String)document.get("username"));
 				users.add(user);
-				
+
 		}
-		
+
 		return users;
-		
-		
+
+
 	}
-	
+
 	public int deleteTrips(String city, LocalDate fromDate, LocalDate toDate) {
 		Bson filter = and(eq("city", city), gte("time.timestamp_start", fromDate), lte("time.timestamp_start", toDate));
 		DeleteResult result = database.getCollection("trip").deleteMany(filter);
 		return (int)result.getDeletedCount();
 	}
-	
+
 	public List<String> getCities(){
 		List<String> cities = new ArrayList<String>();
-		
+
 		MongoCursor<String> cursor = database.getCollection("station").distinct("city", String.class).iterator();
 		while(cursor.hasNext()) {
 			cities.add(cursor.next());
 		}
-		
+
 		return cities;
 	}
-	
+
 	/*public List<Integer> getYears(){
 		List<Integer> years = new ArrayList<Integer>();
-		
+
 		ArrayList<Field<?>> addYear = new ArrayList<Field<?>>();
 		//addYear.add(new Field<Document>("year",new Document("$year","$time.timestamp_start")));
-		
+
 		List<Bson> pipeline = Arrays.asList(
 				//Add Year Field
 				Aggregates.addFields(addYear),
@@ -610,121 +607,121 @@ public class DatabaseManager {
 				//Sort ASC by year
 				Aggregates.sort(new Document("year",1))
 		);
-		
-		
+
+
 		MongoCollection<Document> collection = database.getCollection("trip").withReadPreference(ReadPreference.secondaryPreferred());
-		
+
 		MongoCursor<Document> cursor = collection.aggregate(pipeline).iterator();
-		
-		
+
+
 		while(cursor.hasNext()) {
 			years.add((Integer) cursor.next().get("year"));
 		}
-		
+
 		cursor.close();
-		
-		
+
+
 		return years;
 	}*/
-	
+
 	public List<Integer> getYears(){
 
 		List<Integer> years = new ArrayList<Integer>();
 		MongoCursor<Integer> cursor = database.getCollection("trip").distinct("time.year", Integer.class).iterator();
-		
+
 		while(cursor.hasNext()) {
-			int year = cursor.next();									
+			int year = cursor.next();
 			years.add(year);
 		}
-		
+
 		return years;
-		
+
 	}
-	
+
 	public boolean insertUser(User user) {
 		ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
 		try {
 			String json = ow.writeValueAsString(user);
 			return insertDocument(json,"user");
-			
+
 		} catch (Exception e) {
 			e.printStackTrace();
 			return false;
-		} 
-				
+		}
+
 	}
-	
+
 	public boolean promoteUser(User user) {
 		Document filter = new Document("username",user.getUsername());
 		String currentLevel = (String)user.getStatus();
 		String newLevel = null;
-		
+
 		if(currentLevel.equals("A")) {
 			return false;
 		} else if(currentLevel.equals("S")) {
 			newLevel = "C";
 		} else if(currentLevel.equals("C")) {
-			return false; 
+			return false;
 		}
-		
+
 		List<Bson> pipeline = Arrays.asList(
 		        Aggregates.addFields(new Field<String>("status", newLevel))
 		);
-		
+
 		try {
 			database.getCollection("user").updateOne(filter, pipeline);
 		} catch (Exception e) {
 			e.printStackTrace();
 			return false;
 		}
-		
+
 		return true;
-		
+
 	}
-	
+
 	public boolean demoteUser(User user) {
 		Document filter = new Document("username",user.getUsername());
 		String currentLevel = (String)user.getStatus();
 		String newLevel = null;
-		
+
 		if(currentLevel.equals("A")) {
 			return false;
 		} else if(currentLevel.equals("C")) {
 			newLevel = "S";
 		} else if(currentLevel.equals("S")) {
-			return false; 
+			return false;
 		}
-		
+
 		List<Bson> pipeline = Arrays.asList(
 				Aggregates.addFields(new Field<String>("status",newLevel))
 		);
-		
+
 		try {
 			database.getCollection("user").updateOne(filter, pipeline);
 		} catch(Exception e) {
 			e.printStackTrace();
 			return false;
 		}
-		
+
 		return true;
 	}
-	
+
 	public boolean fire(User user) {
-		
+
 		Bson filter = eq("username",user.getUsername());
 		String currentLevel = (String)user.getStatus();
-		
+
 		if(currentLevel.equals("A")) {
 			System.out.println("Firing is not permitted");
 			return false;
-		
+
 			}
 		else {
 			DeleteResult result = database.getCollection("user").deleteOne(filter);
 			if( (int)result.getDeletedCount()==0)
-				return false;	
+				return false;
 		}
-		
+
 		return true;
 	}
 
@@ -732,7 +729,7 @@ public class DatabaseManager {
 		Document filter = new Document("username", user.getUsername());
 
 		List<Bson> pipeline = Arrays.asList(Aggregates.addFields(new Field<String>("password", newPassword)));
-		
+
 		try {
 			database.getCollection("user").updateOne(filter, pipeline);
 		} catch (Exception e) {
@@ -755,12 +752,12 @@ public class DatabaseManager {
 				new Document("$match", new Document("week", week).append("year", year)),
 				new Document("$group", new Document("_id", "$dow").append("count", new Document("$sum", 1)))
 		);
-		
+
 		AggregateIterable<Document> output = database.getCollection("trip").aggregate(pipeline);
 		System.err.println("[D] Done");
-		
+
 		MongoCursor<Document> cursor = output.cursor();
-		
+
 		List<Document> list = new ArrayList<Document>();
 
 		while (cursor.hasNext()) {
@@ -770,7 +767,7 @@ public class DatabaseManager {
 		cursor.close();
 
 		return list;
-	}	
-	
+	}
+
 
 }
